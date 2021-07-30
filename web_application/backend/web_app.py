949663b5d9a9eb76e7388c5265397f10d672ad1c
KonstantinChernyak <chernyakonstantin@gmail.com>
2021-07-30 17:15:49 +0300

    Добавить вывод последних 10 изображений из базы

--- web_application/backend/web_app.py
+++ web_application/backend/web_app.py
@@ -15,31 +15,29 @@ class WebApplication(Flask):
         self._db_handler = DBHandler(db_name, db_user, db_password, db_host, db_port)
         self._stream_receiver = TcpClient(stream_host, stream_port)
         self._db_handler.connect()
+        self._last_ten_images = None
         super().__init__(*args, **kwargs)
         self.route("/")(self._index)
         self.route("/video_feed")(self._video_feed)
-        self.route("/db")(self._get_db_images)
+        self.route("/db/<int:img_index>")(self._get_db_images)
         logging.debug('Server is ready')
 
     def _index(self):
+        self._last_ten_images: list = self._db_handler.get_last_n_images(10)
         return render_template('index.html')
 
     def _get_video_stream(self) -> bytes:
         while True:
-            try:
-                frame: bytes = self._stream_receiver.receive()
-            except:
-                frame = b''
+            frame: bytes = self._stream_receiver.receive()
             yield b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
 
-    # TODO: Исправить, чтобы возвращался не 1, а 10 элементов
-    def _get_db_images(self):
-        images = self._db_handler.get_last_n_images(10)
-        responses = []
-        for image in images:
-            response_str = b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n'
-            responses.append(Response(response_str, mimetype='multipart/x-mixed-replace; boundary=frame'))
-        return responses[-1]
+    def _get_db_images(self, img_index):
+        try:
+            image = self._last_ten_images[img_index]
+        except IndexError:
+            image = b''
+        response_str = b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n'
+        return Response(response_str, mimetype='multipart/x-mixed-replace; boundary=frame')
 
     def _video_feed(self):
         return Response(self._get_video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
